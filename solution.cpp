07106cf45f75a5c2fe47c68a6e04feed0545c589
Samuel Rodgers <samuel.rodgers.24@cnu.edu>
2025-09-19 20:13:24 -0400

    basic plan

--- solution.cpp
+++ solution.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 
 char* process(const char* numbers) {
-    // TODO implement method
+
+    // TODO set up variables
+
+    // TODO convert char* to numbers
+    //  - This probably needs to be dynamically sized
+
+    // TODO write loop to add reverse
+    //  - Do this for one number first, then all of them
+
+    // TODO combine counters and final values
     return NULL;
 }
