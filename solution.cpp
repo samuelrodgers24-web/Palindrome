96c2581834267abf010ef3532c79a84dafa60526
Samuel Rodgers <samuel.rodgers.24@cnu.edu>
2025-09-19 21:20:42 -0400

    converting argument to int vector

--- solution.cpp
+++ solution.cpp
@@ -1,15 +1,33 @@
+#include <cstring>
 #include <iostream>
+#include <vector>
+using namespace std;
 
 char* process(const char* numbers) {
 
     // TODO set up variables
+    vector<int> nums;
+    vector<int> counts;
+    char charNums[strlen(numbers)+1];
+    strcpy(charNums, numbers);
 
     // TODO convert char* to numbers
     //  - This probably needs to be dynamically sized
+
+    char* first = strtok(charNums, " ");
+    while (first != NULL)
+    {
+        int firstInt = atoi(first);
+        nums.push_back(firstInt);
+        first = strtok (NULL, " ");
+    }
 
     // TODO write loop to add reverse
     //  - Do this for one number first, then all of them
 
     // TODO combine counters and final values
+
+    // TODO delete anything that needs to be deleted
+
     return NULL;
 }
