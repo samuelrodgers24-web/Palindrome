75c6797e18aa30d287d5173927045f3c20737b2d
Samuel Rodgers <samuel.rodgers.24@cnu.edu>
2025-09-19 22:14:43 -0400

    wrote isPalindrome function

--- solution.cpp
+++ solution.cpp
@@ -1,19 +1,43 @@
+#include <algorithm>
 #include <cstring>
 #include <iostream>
+#include <ranges>
 #include <vector>
 using namespace std;
 
-char* process(const char* numbers) {
+bool isPalindrome(int num) {
+    if (num < 10) { return true;}
+    string val = to_string(num);
+    string firstHalf;
+    string secondHalf;
 
-    // TODO set up variables
+    if (val.length() % 2 != 0) {
+        firstHalf = val.substr(0, val.length() / 2);
+        secondHalf = val.substr((val.length() / 2)+1);
+    } else {
+        firstHalf = val.substr(0, val.length() / 2);
+        secondHalf = val.substr((val.length() / 2));
+    }
+    printf("First Half: %s   |   ", firstHalf.c_str());
+    printf("Second Half: %s\n", secondHalf.c_str());
+    reverse(secondHalf.begin(), secondHalf.end());
+    return firstHalf == secondHalf;
+}
+
+char* process(const char* numbers) {
+    // handle empty argument
+    if (strlen(numbers) < 1) {
+        //char* s_out[] = ;
+        return NULL;
+    }
+
+    // set up variables
     vector<int> nums;
     vector<int> counts;
     char charNums[strlen(numbers)+1];
     strcpy(charNums, numbers);
 
-    // TODO convert char* to numbers
-    //  - This probably needs to be dynamically sized
-
+    // convert char* to numbers
     char* first = strtok(charNums, " ");
     while (first != NULL)
     {
@@ -24,10 +48,14 @@ char* process(const char* numbers) {
 
     // TODO write loop to add reverse
     //  - Do this for one number first, then all of them
+    counts.push_back(0);
+    counts[0] += 1;
+    while (!isPalindrome(nums[0]) && counts[0]<100) {}
 
     // TODO combine counters and final values
 
     // TODO delete anything that needs to be deleted
-
     return NULL;
 }
+
+
